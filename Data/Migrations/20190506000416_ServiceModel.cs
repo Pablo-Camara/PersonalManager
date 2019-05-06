8e2ce49a9e9be1a3c9f44869ab12dd13039b6cbf
Pablo Camara <pablo.camara@contentignition.com>
2019-05-06 01:04:58 +0100

    dotnet ef migrations add

---
+++ Data/Migrations/20190506000416_ServiceModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PersonalManager.Data.Migrations
+{
+    public partial class ServiceModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
