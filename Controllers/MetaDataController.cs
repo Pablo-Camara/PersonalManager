2dfdca320c89a228d1fe46169447ec5c73134e64
Pablo Camara <pablo.camara.pt@gmail.com>
2019-05-10 13:20:14 +0100

    Protect MetaData Controller

--- Controllers/MetaDataController.cs
+++ Controllers/MetaDataController.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PersonalManager.Data.Models.MetaData;
 
 namespace PersonalManager.Controllers
 {
     [Route("MetaData")]
+    [Authorize]
     public class MetaDataController : Controller
     {
 
